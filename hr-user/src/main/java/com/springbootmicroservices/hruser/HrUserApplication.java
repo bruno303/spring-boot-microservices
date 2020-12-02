c8a8d2f24c4cc5eeaf2c9c68f37ceb2a10b809e3
Bruno Oliveira <brunooliveira812@gmail.com>
2020-12-01 22:23:27 -0300

    Database seeding

--- hr-user/src/main/java/com/springbootmicroservices/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/springbootmicroservices/hruser/HrUserApplication.java
@@ -1,15 +1,28 @@
 package com.springbootmicroservices.hruser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
+	private final Logger LOGGER = LoggerFactory.getLogger(HrUserApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bcriptPassword(BCryptPasswordEncoder passwordEncoder) {
+		return args -> {
+			LOGGER.info("BCRYPT = {}", passwordEncoder.encode("123456"));
+		};
+	}
 }
