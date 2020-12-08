d334a4768b1fe5f2b4b6ba28abb3f69b4adc3510
Bruno Oliveira <brunooliveira812@gmail.com>
2020-12-08 14:00:33 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/springbootmicroservices/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,42 @@
+package com.springbootmicroservices.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
+	private static final String[] OPERATOR = { "/hr-worker/**" };
+	private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**" };
+
+	private final JwtTokenStore jwtTokenStore;
+//	private final JwtAccessTokenConverter accessTokenConverter;
+
+	public ResourceServerConfig(JwtTokenStore jwtTokenStore, JwtAccessTokenConverter accessTokenConverter) {
+		this.jwtTokenStore = jwtTokenStore;
+//		this.accessTokenConverter = accessTokenConverter;
+	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(jwtTokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(PUBLIC).permitAll()
+			.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+			.antMatchers(ADMIN).hasRole("ADMIN")
+			.anyRequest().authenticated();
+	}
+
+}
